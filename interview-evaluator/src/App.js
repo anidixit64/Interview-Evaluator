cf06afae3e513a83c3a2eab8338979297c560204
Aniket Dixit <anidixit@umich.edu>
2025-03-14 14:40:12 -0400

    Added empty spacer and changed color scheme

--- interview-evaluator/src/App.js
+++ interview-evaluator/src/App.js
@@ -1,35 +1,13 @@
 import React from "react";
+import "./App.css"; // Import external CSS
 
 function App() {
   return (
-    <div style={styles.page}>
-      <div style={styles.centeredText}>Interview Evaluator</div>
-      <div style={styles.spacer}></div>
+    <div className="container">
+      <div className="centeredText">Interview Evaluator</div>
+      <div className="spacer"></div> {/* Extra scroll space */}
     </div>
   );
 }
-
-const styles = {
-  page: {
-    textAlign: "center",
-    fontFamily: "Arial, sans-serif",
-    height: "200vh", // Creates scrollable space
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  centeredText: {
-    fontSize: "3rem",
-    fontWeight: "bold",
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)", // Ensures perfect centering
-  },
-  spacer: {
-    height: "100vh", // Extra scrollable space below
-  },
-};
 
 export default App;
